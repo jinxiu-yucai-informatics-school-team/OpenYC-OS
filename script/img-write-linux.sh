15d966803ca6548235741ef9ca370d2eea99634f
xk2013 <hzk201312@163.com>
2024-01-15 22:43:59 +0800

    Updated img-write-linux.sh

--- script/img-write-linux.sh
+++ script/img-write-linux.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2024-present Jinxiu Yucai Informatics School Team
+# All rights reserved.
+#
+# Create Time: 2024/01/15
+# Author: xk2013
+# E-Mail: hzk201312@163.com
+#
+
 if [-f "disk1.vhd"]; then
     mv disk1.vhd disk1.img
 fi
